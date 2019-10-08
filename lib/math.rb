39f280b483a0f1b07b77205c5d542b8eff75a3bf
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-08 15:01:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-
+5+4
 end
 
 def subtraction(num1, num2)
-
+10-5
 end
 
 def division(num1, num2)
-
+50/2
 end
 
 def multiplication(num1, num2)
-
+4*30
 end
 
 def modulo(num1, num2)
-
+  34/7
 end
 
 def square_root(num)
+  num.math.sgrt = (81)
+
 
 end
